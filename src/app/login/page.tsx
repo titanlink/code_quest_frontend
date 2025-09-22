1cbcb05867f8959f55b642edf2b2620d2fa0e1e1
nativolink <ingluisrosario15@gmail.com>
2025-09-22 15:46:41 -0400

    Mejora de diseño del login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
+import DarkVeil from "@/components/DarkVeil"
 import LoginForm from "@/components/login/login-form"
+import { Meteors } from "@/components/ui/meteors"
 import type React from "react"
 
 
 export default function LoginPage() {
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-sky-700/20 to-accent/10 px-4">
-      <LoginForm />
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-sky-700/20 to-accent/10 px-4 relative">
+      <Meteors />
+      <DarkVeil speed={0.3} hueShift={39} />
+      <div className="inset-0 absolute flex items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
   )
 }
